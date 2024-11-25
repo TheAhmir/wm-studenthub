dc695b41cab5683de0bcfc7106070f3b2582ac15
Ahmir Postell <postell.ahmir@gmail.com>
2024-11-25 00:53:38 -0500

    changed frontend. testing on deployment

---
+++ client/src/components/HomeView/HomeView.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomeView = () => {
+    return (
+        <div>
+            <h1>This is the Home Screen</h1>
+            <p>I hope this works</p>
+        </div>
+    )
+}
+
+export default HomeView;
