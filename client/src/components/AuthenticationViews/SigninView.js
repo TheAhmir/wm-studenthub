ff6986b5ecfa549e2804ad9e0f1c18a3165f063c
Ahmir Postell <postell.ahmir@gmail.com>
2024-12-15 21:28:34 -0500

    css and code, code cleanup, and ui change in signup

--- client/src/components/AuthenticationViews/SigninView.js
+++ client/src/components/AuthenticationViews/SigninView.js
@@ -1,45 +1,36 @@
 import React, { useEffect, useState } from "react";
 import './Auth.scss';
 import { useNavigate } from "react-router-dom";
-import { auth } from "../FirebaseAuth/firebase";
-import { trackUserChanges } from "../FirebaseAuth/AuthMethods";
-import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { trackUserChanges, isAcceptableEmail, signIn } from "../FirebaseAuth/AuthMethods";
 
-const SigninView = () => {
+const SignInView = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [user, setUser] = useState(null);
     const navigate = useNavigate();
 
 const handleSignIn = async (event) => {
     event.preventDefault();
 
-    if (!email || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-        alert('Please provide a valid email address.');
-        return;
-    }
-
-    signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    navigate('/')
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    alert(errorMessage)
-  });
-
-    
+    isAcceptableEmail(email, (isValid) => {
+        if (isValid) {
+            signIn(email, password, (result) => {
+                if (result) {
+                    navigate('/')
+                } else {
+                    alert('Failed to sign in. Try again.')
+                }
+            })
+        } else {
+            alert('Please provide a valid email address.')
+        }
+    })
 };
 
 
     useEffect(() => {
         // Call trackUserChanges and pass a callback to update state
-        const unsubscribe = trackUserChanges((currentUser) => {
-            setUser(currentUser); // Update the user state
+        const unsubscribe = trackUserChanges(({/*currentuser*/}) => {
+            //setUser(currentUser); // Update the user state
         });
 
         // Clean up the subscription when the component unmounts
@@ -48,11 +39,7 @@ const handleSignIn = async (event) => {
 
     return (
         <div className="auth-page">
-            {user ? (
-                <p>Signed in as: {user.email}</p>
-            ) : (
-                <>
-                    <form className="auth-form" onSubmit={handleSignIn}>
+            <form className="auth-form" onSubmit={handleSignIn}>
                         <h1>Sign In</h1>
                         <input
                             className="auth-item"
@@ -62,6 +49,7 @@ const handleSignIn = async (event) => {
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
                             required
+                            
                         />
                         <input
                             className="auth-item"
@@ -77,13 +65,10 @@ const handleSignIn = async (event) => {
                         </button>
                         <div className="other-actions">
                             <a href="/auth/forgot-password">Forgot password?</a>
-                            <a href="/auth/signup">Signup</a>
                         </div>
                     </form>
-                </>
-            )}
         </div>
     );
 };
 
-export default SigninView;
+export default SignInView;
