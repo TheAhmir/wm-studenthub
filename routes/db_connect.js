1a0331527a4efd007346bb64640b4dd08cfdda9c
Ahmir Postell <postell.ahmir@gmail.com>
2025-01-13 05:31:50 -0500

    testing123

---
+++ routes/db_connect.js
@@ -0,0 +1,24 @@
+// routes/db_connect.js
+const express = require('express');
+const router = express.Router();
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const MONGODB_URI = process.env.AZURE_COSMOS_CONNECTIONSTRING;
+
+router.get('/data', async (req, res) => {
+    try {
+        const client = new MongoClient(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        await client.connect();
+        const db = client.db("wm-studenthub-database"); // Make sure this matches your database name
+        const collections = await db.listCollections().toArray();
+        const collectionNames = collections.map(col => col.name);
+        await client.close();
+        res.status(200).json(collectionNames);
+    } catch (error) {
+        res.status(500).send("Error connecting to MongoDB");
+        console.error(error);
+    }
+});
+
+module.exports = router;
