9f789f5b7cfec8f9244f1b08f6fb9ac7c050c792
Ahmir Postell <postell.ahmir@gmail.com>
2025-01-13 05:31:50 -0500

    testing123

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var { spawn } = require('child_process');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var dbRouter = require('./routes/db_connect');
 
 var app = express();
 
@@ -14,22 +15,20 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// Serve static files from the React app
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/data', dbRouter); // Adjusted route
 
 // Catch-all handler for any requests that don't match routes above
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
