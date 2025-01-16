63a8a5fd69466d0391b16eb5f432e44f664b5968
DripGodMod <madicemod@gmail.com>
2025-01-16 16:36:44 -0500

    Testing

--- client/src/components/HomeView/HomeView.js
+++ client/src/components/HomeView/HomeView.js
@@ -8,6 +8,7 @@ const HomeView = () => {
     return (
         <div>
             <h1>This is the Home Screen</h1>
+            <h2>Hello world</h2>
             <p>I hope this works</p>
         </div>
     )
